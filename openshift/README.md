679d2df14ac5bbf4c8e27dbe95848b90ccad062f
julianiff <julian.iff@helsana.ch>
2020-08-04 13:32:10 +0200

    F Added miniShift to dotfile setup

--- openshift/README.md
+++ openshift/README.md
@@ -1,2 +1,6 @@
 Semi-Curated list of Openshift / Cubernetes Development tools 
 for macOS
+
+## Local Kubernetes Development
+- Skaffold (Workflow for building, pushing and deploying the application)
+- 
--- openshift/local.sh
+++
@@ -1,8 +0,0 @@
-if brew ls --versions skaffold > /dev/null; then
-	echo "Installed version found"
-	echo "Updating..."
-	brew update skaffold  
-else
-	echo "Cask not found, installing skallold" 
-	brew install skaffold
-fi
